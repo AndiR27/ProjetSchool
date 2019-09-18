b86b5b8f3c0c12a02a2981cfdeee4eac21cd1c88
AndiR27 <55502231+AndiR27@users.noreply.github.com>
2019-09-18 18:14:59 +0200

    first commit

---
+++ Test.py
@@ -0,0 +1,29 @@
+print(1 + 1)
+print(3 / 2)
+print(3 // 2)
+print( 31 % 2)
+
+
+x = 5000 - ((35 * 54.5) + (40 * 35))
+print(x)
+
+y = x - (150 * 5)
+print(y)
+
+z = y -(29 * 12.5)
+
+print(z)
+
+if z > 0: print("Il reste" ,z, "kg de libre")
+
+Audrey: int = 17
+Marie = (Audrey * 2) + 6.75
+Max = Marie - 9.27
+Laura = Max - 7.29
+Jean = Audrey + 8
+
+print("distance parcourue par Marie :",Marie, "km")
+print("distance parcourue par Max :",Max, "km")
+print("distance parcourue par Laura :",Laura, "km")
+print("distance parcourue par Audrey :",Audrey, "km")
+print("distance parcourue par Jean :",Jean, "km")
